dc28ba9e6573aab9a22b424f6aea57d2f7f52118
karmariv <karmariv82@gmail.com>
2024-04-16 20:46:02 -0600

    latest version of main.py

--- main.py
+++ main.py
@@ -1,19 +1,14 @@
 # This entrypoint file to be used in development. Start by reading README.md
 import budget
 from budget import create_spend_chart
-#from unittest import main
-
-
 
 food = budget.Category("Food")
 food.deposit(1000, "initial deposit")
 food.deposit(50, "additional expenses")
 food.withdraw(10.15, "groceries")
 food.withdraw(15.89, "restaurant and more food for dessert")
-print(food.get_balance())
 clothing = budget.Category("Clothing")
 food.transfer(50, clothing)
-print(food.get_balance())
 clothing.withdraw(25.55)
 clothing.withdraw(100)
 auto = budget.Category("Auto")
@@ -24,6 +19,3 @@ print(food)
 print(clothing)
 create_spend_chart([food, clothing, auto])
 print(create_spend_chart([food, clothing, auto]))
-
-# Run unit tests automatically
-#main(module='test_module', exit=False)
